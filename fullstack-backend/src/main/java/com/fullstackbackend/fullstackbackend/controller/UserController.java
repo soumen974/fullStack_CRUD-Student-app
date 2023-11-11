2938dc11a210d5ffa6b88d9fef4ca8146668fbf0
soumen bhunia <sob99338@gmail.com>
2023-11-11 19:03:39 +0530

    backend connected to frontend

--- fullstack-backend/src/main/java/com/fullstackbackend/fullstackbackend/controller/UserController.java
+++ fullstack-backend/src/main/java/com/fullstackbackend/fullstackbackend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.fullstackbackend.fullstackbackend.controller;
 import com.fullstackbackend.fullstackbackend.model.User;
 import com.fullstackbackend.fullstackbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin ("http://localhost:3000")
 public class UserController {
 
     @Autowired
