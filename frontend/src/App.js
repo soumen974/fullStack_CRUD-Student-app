31ffeb9f0a2deb4c3a81f277e6dbc8eeb254d973
soumen bhunia <sob99338@gmail.com>
2023-11-11 19:02:20 +0530

    router added to pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import Navbar from "./Component/Navbar";
+import Home from './Pages/Home';
+import { BrowserRouter as Router, Route, Routes, Link, Outlet } from 'react-router-dom';
+import UserLayout from './Pages/UserLayout';
+import AddUser from "./Users/AddUser";
 
 function App() {
   return (
     <>
+       <Router>
+         <Routes>
+         <Route path="/" element={<UserLayout/>} >
+         <Route path="/" element={<Home/>} />
+         <Route path="/addnewuser" element={<AddUser/>} />
+         </Route>
+         </Routes>
+      </Router>
     </>
   );
 }
